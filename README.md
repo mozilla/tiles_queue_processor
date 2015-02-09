18ba6bc62c8b82860382324f123f6e8f29485d11
Daniel Thornton <dthornton@mozilla.com>
2015-02-09 12:01:07 -0800

    intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Overview
+===
+
+Script for pulling blobs from s3/sns/sqs queue, splitting them by date, and
+pushing them into ddfs
---
+++ processor.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+
+import boto
+import boto.sqs
+import boto.sqs.queue
+import json
+import os
+import signal
+import yaml
+
+from subprocess import call
+
+
+class Config:
+    sqs_arn = 'arn:aws:sqs:us-east-1:0123456789012:mozilla-tiles-queue'
+    print_config = False
+    ddfs_master = 'disco://localhost'
+    tag_prefix = 'incoming:'
+
+    def __init__(self, config_file='processor.yaml'):
+        if os.path.isfile(config_file):
+            with open(config_file) as pointer:
+                config = yaml.load(pointer.read())
+            if config is None:
+                return
+            for key, value, in config.items():
+                if getattr(self, key) != value:
+                    setattr(self, key, value)
+                if self.print_config:
+                    print(self.__dict__)
+
+
+sigint = signal.getsignal(signal.SIGINT)
+stop = False
+
+
+def sigint_later(signal, frame):
+    global stop
+    stop = (signal, frame)
+
+
+def unstoppable():
+    signal.signal(signal.SIGINT, sigint_later)
+
+
+def stoppable():
+    global stop
+    if stop:
+        raise KeyboardInterrupt
+    else:
+        signal.signal(signal.SIGINT, sigint)
+
+
+s3 = boto.connect_s3()
+
+
+def pull_from_s3(bucket, key, destination):
+    global s3
+    print('Downloading from s3://%s/%s to %s' % (bucket, key, destination))
+    s3bucket = s3.get_bucket(bucket, validate=False)
+    s3key = s3bucket.get_key(key)
+    if s3key:
+        s3key.get_contents_to_filename(destination)
+    else:
+        raise Exception('Error: key not found: s3://%s/%s' % (bucket, key))
+
+
+def split_by_date(path):
+    blobs = set()
+    pointers = {}
+    try:
+        with open(path) as pointer:
+            for line in pointer:
+                try:
+                    date = json.loads(line)['date']
+                except:
+                    # failed to get date from line, use default
+                    date = '0000-00-00'
+                blob = path + '.' + date
+                # get the open file pointer for blob
+                pointer2 = pointers.get(date)
+                if pointer2 is None:
+                    pointer2 = open(blob, 'w')
+                    pointers[date] = pointer2
+                # write this log line to blob
+                pointer2.write(line)
+                # keep track of the blobs
+                blobs.add((date, blob))
+    finally:
+        for pointer in pointers.itervalues():
+            pointer.close()
+    return blobs
+
+
+def push_to_ddfs(blobs, master, tag_prefix, label):
+    for blob in blobs:
+        command = [
+            '/usr/bin/ddfs',
+            '--master',
+            master,
+            'chunk',
+            tag_prefix + label + ':' + blob[0],
+            blob[1]
+            ]
+	print('Executing: ' + ' '.join(command))
+	call(command)
+
+
+def clean(path, blobs):
+    os.remove(path)
+    for _, blob in blobs:
+        os.remove(blob)
+
+
+def process(message, body, ddfs_master, tag_prefix):
+    destination = '/tmp/' + body['path'].replace("/", ".")
+    pull_from_s3(body['bucket'], body['path'], destination)
+    blobs = split_by_date(destination)
+    unstoppable()
+    push_to_ddfs(blobs, ddfs_master, tag_prefix, body['label'])
+    message.delete()
+    stoppable()
+    clean(destination, blobs)
+
+
+def main(config=Config()):
+    try:
+        _, _, _, sqs_region, _, sqs_queue = config.sqs_arn.split(':', 5)
+    except Exception:
+        raise Exception('invalid sqs arn')
+
+    sqs = boto.sqs.connect_to_region(sqs_region)
+    queue = sqs.get_queue(sqs_queue)
+
+    if queue is None:
+        raise Exception('could not connect to sqs queue: ' + config.sqs_arn)
+
+    while True:
+        message = queue.read(wait_time_seconds=20)
+        if message is None:
+            #print('Queue is empty')
+            continue
+        try:
+            raw_body = message.get_body()
+            print('Message received')
+        except Exception:
+            print('Failed to get message body')
+            continue
+        try:
+            body = json.loads(json.loads(raw_body)['Message'].replace("'",'"'))
+        except Exception:
+            print('Invalid message body: ' + raw_body)
+            continue
+        try:
+            print('Processing: ' + str(body))
+            process(message, body, config.ddfs_master, config.tag_prefix)
+        except Exception:
+            print('Failed to process: ' + str(body))
+            stoppable()
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    args = argv[1:]
+
+    if '-h' in args or '--help' in args:
+        print('Usage: %s [config.yaml]' % argv[0])
+    elif args:
+        main(Config(args[0]))
+    else:
+        main()
